4755346c673bd2eb86eb87723e439af6a12c45b1
Antonio Terpin <antonio.terpin@gmail.com>
2020-12-15 01:45:31 +0100

    fix plot current image after merge

--- src/plot/plotCurrentImage.m
+++ src/plot/plotCurrentImage.m
@@ -9,11 +9,7 @@ function plotCurrentImage (image, inliers, outliers)
         scatter(inliers(:, 1), inliers(:, 2), 15, 'g', 'filled');
     end
     if(~isempty(outliers))
-<<<<<<< HEAD:src/plot/plotCurrentImage.m
         scatter(outliers(1, :), outliers(2, :), 15, 'r', 'filled');
-=======
-        scatter(outliers(2, :), outliers(1, :), 15, 'r', 'filled');
->>>>>>> dbabe5a106c153f1ef9ed7e5182053ddc2780624:plot/plotCurrentImage.m
     end
     hold off;
     title('Current Image');
