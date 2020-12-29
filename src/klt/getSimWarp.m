52a4412cca3bdfdfa828e210f441365d87a1642e
Antonio Terpin <antonio.terpin@gmail.com>
2020-12-29 10:04:04 +0100

    Improved KLT functions

--- src/klt/getSimWarp.m
+++ src/klt/getSimWarp.m
@@ -1,11 +1,10 @@
 function W = getSimWarp(dx, dy, alpha_deg, lambda)
 % alpha given in degrees, as indicated
+    
+    alpha = deg2rad(alpha_deg);
+    c = cos(alpha);
+    s = sin(alpha);
 
-alpha_rad = alpha_deg * pi / 180;
-c = cos(alpha_rad);
-s = sin(alpha_rad);
-R = [c -s; s c];
-W = lambda * [R [dx; dy]];
+    W = lambda * [c, -s, dx; s, c, dy];
 
 end
-
--- src/klt/getWarpedPatch.m
+++ src/klt/getWarpedPatch.m
@@ -1,41 +1,38 @@
 function patch = getWarpedPatch(I, W, x_T, r_T)
-% x_T is 1x2 and contains [x_T y_T] as defined in the statement. patch is
-% (2*r_T+1)x(2*r_T+1) and arranged consistently with the input image I.
 
-patch = zeros(2*r_T+1);
+width = size(I,2); 
+height = size(I,1);
 
-interpolate = true;
+[Idx, Jdx] = meshgrid(-r_T:r_T, -r_T:r_T);
+numPix = numel(Idx);
+w_idx = repmat(x_T.', 1, numPix) + W * [Idx(:).'; Jdx(:).'; ones(1, numPix)];
+w_idx_ = floor(w_idx);
 
-% Somehow pre-calculating this resulted in major speedup.
-max_coords = fliplr(size(I));
-WT = W';
+idx = r_T + [Idx(:).'; Jdx(:).'] + 1;
 
-for x = -r_T:r_T
-    for y = -r_T:r_T
-        pre_warp = [x y];
-        warped = x_T + [pre_warp 1] * WT;
-        if all(warped < max_coords & warped > [1 1])
-            % Don't forget that patch coefficients are 1:(2*r_T+1), rather
-            % than -r_T:r_T.
-            if interpolate
-                floors = floor(warped);
-                weights = warped - floors;
-                a = weights(1); b = weights(2);            
-                intensity = (1-b) * (...
-                    (1-a) * I(floors(2), floors(1)) +...
-                    a * I(floors(2), floors(1)+1))...
-                    + b * (...
-                    (1-a) * I(floors(2)+1, floors(1)) +...
-                    a * I(floors(2)+1, floors(1)+1));
-                patch(y + r_T + 1, x + r_T + 1) = ...
-                    intensity;
-            else
-                patch(y + r_T + 1, x + r_T + 1) = ...
-                    I(int32(warped(2)), int32(warped(1)));
-            end
-        end
-    end
+% keep only values warped within image boundaries
+valid = all(w_idx_ > 1 & w_idx_ < [width; height], 1);
+w_idx = w_idx(:, valid);
+w_idx_ = w_idx_(:, valid);
+idx = idx(:, valid);
+
+patchSize = 2*r_T.' + 1;
+patch = zeros(patchSize);
+
+% this is an approx
+% I = double(padarray(I, [1 1], 'replicate')); % easier bilinear interpolation
+% % Build warped patch
+ab = (w_idx - w_idx_).'; % bilinear interpolation coefficients
+
+% % For convenience, we use linear indexing
+I = double(I);
+idx = sub2ind(size(patch), idx(2,:).', idx(1,:).');
+shift = ones(size(w_idx_,2), 1);
+w_idx_00 = sub2ind(size(I), w_idx_(2,:).', w_idx_(1,:).');
+w_idx_01 = w_idx_00 + size(I,1)*shift; % sub2ind(size(I), w_idx_(2,:).'+1, w_idx_(1,:).');
+w_idx_10 = w_idx_00 + shift; % sub2ind(size(I), w_idx_(2,:).', w_idx_(1,:).'+1);
+w_idx_11 = w_idx_01 + shift; % sub2ind(size(I), w_idx_(2,:).'+1, w_idx_(1,:).'+1);
+
+patch(idx) = (1-ab(:,2)) .* ((1-ab(:,1)) .* I(w_idx_00) + ab(:,1) .* I(w_idx_01)) ...
+    + ab(:,2) .* ((1-ab(:,1)) .* I(w_idx_10) + ab(:,1) .* I(w_idx_11));
 end
-
-end
-
