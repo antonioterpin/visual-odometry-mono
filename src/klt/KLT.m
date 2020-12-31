1b14d89cb2333fb8559241c3ace1bdb6dd2a8371
Antonio Terpin <antonio.terpin@gmail.com>
2020-12-31 16:07:48 +0100

    KLT multiscale

--- src/klt/KLT.m
+++ src/klt/KLT.m
@@ -1,27 +1,52 @@
-function [trKp,kpMask] = KLT(image1,image2,kp1,r_T,nIt,lambda)
+function [trKp,kpMask] = KLT(image1,image2,kp1,r_T,nIt,lambda,nScales)
 %KLT Summary of this function goes here
 %   Detailed explanation goes here
 
-    % Sampling
-    image1_ = imresize(image1, 0.25); kp1 = kp1 / 4;
-    image2_ = imresize(image2, 0.25);
+    if nargin < 7
+        nScales = 1;
+    end
+    
+    baseDownsampling = 0.25;
+    
+    W0 = reshape(getSimWarp(0, 0, 0, 1), [], 1);
+    W0 = repmat(W0, 1, size(kp1, 2));
 
+    for it = nScales : -1 : 2
+        scale = baseDownsampling^it;
+
+        parfor j = 1:size(kp1, 2)
+            W0_j = reshape(W0(:, j), [2,3]);
+            W0_j = trackKLT(...
+                imresize(image1, scale), imresize(image2, scale), ...
+                kp1(:,j).' * scale, ceil(r_T * scale), nIt, W0_j);
+            W0_j(:, end) = W0_j(:, end) / baseDownsampling;
+            W0(:, j) = reshape(W0_j, [], 1);
+        end
+    end
+    
     dkp = zeros(size(kp1));
     kpMask = true(1, size(kp1, 2));
+    scale = baseDownsampling;
     parfor j = 1:size(kp1, 2)
-        [dkp(:,j), kpMask(j)] = trackKLTRobustly(image1_, image2_, ...
-            kp1(:,j).', r_T, nIt, lambda);
+        W0_j = reshape(W0(:, j), [2,3]);
+        [dkp(:,j), kpMask(j)] = trackKLTRobustly(...
+            imresize(image1, scale), imresize(image2, scale), ...
+            kp1(:,j).' * scale, ceil(r_T * scale), nIt, lambda, W0_j);
     end
-    kpMask = kpMask > 0;
-    trKp = kp1(:,kpMask) + dkp(:,kpMask);
-
+    trKp = kp1(:,kpMask) + dkp(:,kpMask) / scale;
     % Interpolation
-    trKp = trKp * 4;
     
-    % Filter invalid keypoints
     valid = trKp(1,:) >= 1 & trKp(2,:) >= 1 ...
-        & trKp(1,:) < size(image2,2) & trKp(2,:) < size(image2,2);
+        & trKp(1,:) < size(image2,2) & trKp(2,:) < size(image2,1);  %TODO Correct this also in develop  size(image2,1)
     trKp = trKp(:,valid);
     kpMask(kpMask > 0) = valid;
+    
+    kp1 = kp1(:, kpMask);
+    figure(8)
+    imshow(image2);
+    hold on;
+    plotMatches(1:size(trKp, 2), flipud(trKp), flipud(kp1));
+    hold off;
+    pause(0.05);
 end
 
--- src/klt/trackKLTRobustly.m
+++ src/klt/trackKLTRobustly.m
@@ -1,5 +1,5 @@
 function [delta_keypoint, keep] = trackKLTRobustly(I_prev, I, keypoint, ...
-    r_T, num_iters, lambda)
+    r_T, num_iters, lambda, W0)
 % I_prev: reference image, I: image to track point in, keypoint: point to 
 % track, expressed as [x y]=[col row], r_T: radius of patch to track, 
 % num_iters: amount of iterations to run, lambda: bidirectional error
@@ -7,10 +7,11 @@ function [delta_keypoint, keep] = trackKLTRobustly(I_prev, I, keypoint, ...
 % images, (2x1), keep: true if the point tracking has passed the
 % bidirectional error test.
 
-W = trackKLT(I_prev, I, keypoint, r_T, num_iters);
+W0inv = [W0(:,1:2).', -W0(:,1:2).'*W0(:,end)];
+
+W = trackKLT(I_prev, I, keypoint, r_T, num_iters, W0);
 delta_keypoint = W(:, end);
-Winv = trackKLT(I, I_prev, (keypoint'+delta_keypoint)', r_T, ...
-    num_iters);
+Winv = trackKLT(I, I_prev, (keypoint'+delta_keypoint)', r_T, num_iters, W0inv);
 dkpinv = Winv(:, end);
 keep = norm(delta_keypoint + dkpinv) < lambda;
 
